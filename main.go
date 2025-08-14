9d2ce63043459c6423e94829567e39e597717b2a
David Williams <davidw1457@gmail.com>
2025-08-14 09:55:47 -0500

    ch02.l01

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+    "fmt"
+    "net/http"
+    "sync/atomic"
+)
 
 func main() {
     mux := http.NewServeMux()
@@ -10,7 +14,59 @@ func main() {
         Addr: ":8080",
     }
 
-    mux.Handle("/", http.FileServer(http.Dir(".")))
-    
+    cfg := apiConfig{}
+    mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix(
+        "/app",
+        http.FileServer(http.Dir("."),
+    ))))
+    mux.HandleFunc("/healthz", healthCheck)
+    mux.HandleFunc("/metrics", cfg.hitCounter)
+    mux.HandleFunc("/reset", cfg.reset)
+
     server.ListenAndServe()
 }
+
+func healthCheck(rw http.ResponseWriter, _ *http.Request) {
+    rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    rw.WriteHeader(http.StatusOK)
+
+    _, err := rw.Write([]byte("OK"))
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+type apiConfig struct {
+    fileserverHits atomic.Int32
+}
+
+func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func (rw http.ResponseWriter, rq *http.Request) {
+        a.fileserverHits.Add(1)
+        next.ServeHTTP(rw, rq)
+    })
+}
+
+func (a *apiConfig) hitCounter(rw http.ResponseWriter, _ *http.Request) {
+    rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    rw.WriteHeader(http.StatusOK)
+
+    count := fmt.Sprintf("Hits: %d", a.fileserverHits.Load())
+    _, err := rw.Write([]byte(count))
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func (a *apiConfig) reset(rw http.ResponseWriter, _ *http.Request) {
+    a.fileserverHits.Store(0)
+    
+    rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    rw.WriteHeader(http.StatusOK)
+
+    _, err := rw.Write([]byte("counter reset"))
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
